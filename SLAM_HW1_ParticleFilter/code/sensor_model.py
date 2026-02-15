cfc3e491f2e210280c3b169959fe00e3e3e68424
Ayush Sharan <ayush00414@gmail.com>
2026-02-14 21:25:40 -0500

    Implemented the beam_range_finder_model

--- SLAM_HW1_ParticleFilter/code/sensor_model.py
+++ SLAM_HW1_ParticleFilter/code/sensor_model.py
@@ -23,10 +23,10 @@ class SensorModel:
         TODO : Tune Sensor Model parameters here
         The original numbers are for reference but HAVE TO be tuned.
         """
-        self._z_hit = 1
+        self._z_hit = 0.7
         self._z_short = 0.1
         self._z_max = 0.1
-        self._z_rand = 100
+        self._z_rand = 0.1
 
         self._sigma_hit = 50
         self._lambda_short = 0.1
@@ -45,6 +45,8 @@ class SensorModel:
         self.map_size_y = map_obj.get_map_size_y()
         self.map_resolution = self.map_size_x / self.occupancy_map.shape[0]
 
+        self.laser_offset = 25 #in cm
+
     def ray_casting(self, x0, y0, theta_beam):
         step_size = self.map_resolution/2
 
@@ -58,8 +60,8 @@ class SensorModel:
             x_grid = int(np.floor(x/self.map_resolution))
             y_grid = int(np.floor(y/self.map_resolution))
 
-            if(x_grid > self.occupancy_map.shape[1] or x_grid < 0 or \
-               y_grid > self.occupancy_map.shape[0] or y_grid <0):
+            if(x_grid >= self.occupancy_map.shape[1] or x_grid < 0 or \
+               y_grid >= self.occupancy_map.shape[0] or y_grid <0):
                 return self._max_range
             
             if(self.occupancy_map[y_grid, x_grid] > self._min_probability):
@@ -68,6 +70,52 @@ class SensorModel:
             d += step_size
 
         return self._max_range
+
+    def compute_hit_eta(self, z_t_k_star):
+        upper = (self._max_range - z_t_k_star)/self._sigma_hit
+        lower = (0 - z_t_k_star)/self._sigma_hit
+
+        denom = norm.cdf(upper) - norm.cdf(lower)
+        denom = max(denom, 1e-12)
+        eta = 1.0/denom
+
+        return eta
+    
+    def compute_hit_likelihood(self, z_t1, z_t_k_star):
+        p_hit = norm.pdf(z_t1, loc=z_t_k_star, scale=self._sigma_hit)
+        eta = self.compute_hit_eta(z_t_k_star)
+        
+        return eta * p_hit
+    
+    def compute_short_likelihood(self, z_t1, z_t_k_star):
+
+        denom = 1 - np.exp(-self._lambda_short * z_t_k_star)
+        denom = max(denom, 1e-12)
+        eta = 1.0/denom
+        
+        if(z_t1 >= 0 and z_t1 <= z_t_k_star):
+            p_short = eta * self._lambda_short * np.exp(-self._lambda_short * z_t1)
+        else:
+            p_short = 0
+
+        return p_short
+    
+    def compute_max_likelihood(self, z_t1):
+        if (z_t1 == self._max_range):
+            p_max = 1
+        else:
+            p_max = 0
+
+        return p_max
+
+    def compute_rand_likelihood(self, z_t1):
+        
+        if(z_t1 >= 0 and z_t1 < self._max_range):
+            p_rand = 1.0/self._max_range
+        else:
+            p_rand = 0
+
+        return p_rand
 
     def beam_range_finder_model(self, z_t1_arr, x_t1):
         """
@@ -78,10 +126,27 @@ class SensorModel:
         """
         TODO : Add your code here
         """
-        q=1
+
+        prob_zt1 = 0.0 # changed 1.0 -> 0.0 as we are summing log likelihoods
+
+        x_laser = x_t1[0] + self.laser_offset * np.cos(x_t1[2])
+        y_laser = x_t1[1] + self.laser_offset * np.sin(x_t1[2])
+
         for k in range(0, 180, self._subsampling):
-            z_t_k_star = self.ray_casting()
+            angle = -np.pi/2 + k * (np.pi/180)
+            theta_beam = x_t1[2] + angle
 
+            z_t_k_star = self.ray_casting(x_laser, y_laser, theta_beam)
 
-        prob_zt1 = 1.0
+            p_hit = self.compute_hit_likelihood(z_t1_arr[k], z_t_k_star)
+            p_short = self.compute_short_likelihood(z_t1_arr[k], z_t_k_star)
+            p_max = self.compute_max_likelihood(z_t1_arr[k])
+            p_rand = self.compute_rand_likelihood(z_t1_arr[k])
+
+            p = self._z_hit * p_hit + self._z_short * p_short + \
+                self._z_max * p_max + self._z_rand * p_rand
+            
+            p = max(p, 1e-12)
+            prob_zt1 += np.log(p)
+
         return prob_zt1
